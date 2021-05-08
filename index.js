d5546f877b6a6b1522ef66d0206dbe6e7459b96a
Joe Lee <jlee@knitpeople.com>
2021-05-08 15:16:13 -0400

    updated prompts

--- index.js
+++ index.js
@@ -23,84 +23,90 @@ const requiresCoastInput = (territory) =>
 const isActionValid = (action) => Object.keys(actionTypes).includes(action);
 
 rl.question("What is the location unit? ", function (territory) {
-	rl.question("What type of unit is it? ", function (unitType) {
-		if (isTerritoryValid(territory) && !requiresCoastInput(territory)) {
-			rl.question(
-				`What would you like it to do? (M)ove, (H)old, (C)onvoy, (S)upport? `,
-				function (action) {
-					const ordersObj = ordersTemplates(action.toUpperCase());
-					if (isActionValid(action.toUpperCase())) {
-						if (action.toUpperCase() === "H") {
-							ordersObj.origin = territory;
-							ordersObj.unitType = unitType;
-							ordersValidator(ordersObj);
-							rl.close();
-						}
-						if (action.toUpperCase() === "C") {
-							rl.question(
-								`What unit to convoy ${availableMovements(territory)}`,
-								function (origin) {
-									rl.question(
-										`Convoy to where ${availableMovements(
-											territory
-										)} *** still need to build algorithm for convoy chaining`,
-										function (dest) {
-											ordersObj.currentLocation = territory;
-											ordersObj.origin = origin;
-											ordersObj.destination = dest;
-											ordersObj.unitType = unitType;
-											ordersObj.coast = null;
-											ordersObj.actionType = "C";
-											ordersValidator(ordersObj);
-											rl.close();
-										}
-									);
-								}
-							);
-						}
-						if (action.toUpperCase() === "S") {
-							rl.question(`Support what unit?`, function (supportingUnit) {
-								rl.question(`to where? `, function (dest) {
-									ordersObj.origin = territory;
-									ordersObj.destination = dest;
-									ordersObj.supportingUnit = supportingUnit;
-									ordersObj.actionType = dest === territory ? "H" : "M";
-									ordersObj.unitType = unitType;
-									ordersValidator(ordersObj);
-									rl.close();
+	rl.question(
+		"What type of unit is it? (A)rmy or (N)avy ",
+		function (unitType) {
+			if (isTerritoryValid(territory) && !requiresCoastInput(territory)) {
+				rl.question(
+					`What would you like it to do? (M)ove, (H)old, (C)onvoy, (S)upport? `,
+					function (action) {
+						const ordersObj = ordersTemplates(action.toUpperCase());
+						if (isActionValid(action.toUpperCase())) {
+							if (action.toUpperCase() === "H") {
+								ordersObj.origin = territory;
+								ordersObj.unitType = unitType;
+								ordersValidator(ordersObj);
+								rl.close();
+							}
+							if (action.toUpperCase() === "C") {
+								rl.question(
+									`What unit to convoy ${availableMovements(territory)}`,
+									function (origin) {
+										rl.question(
+											`Convoy to where ${availableMovements(
+												territory
+											)} *** still need to build algorithm for convoy chaining`,
+											function (dest) {
+												ordersObj.currentLocation = territory;
+												ordersObj.origin = origin;
+												ordersObj.destination = dest;
+												ordersObj.unitType = unitType;
+												ordersObj.coast = null;
+												ordersObj.actionType = "C";
+												ordersValidator(ordersObj);
+												rl.close();
+											}
+										);
+									}
+								);
+							}
+							if (action.toUpperCase() === "S") {
+								rl.question(`Support what unit?`, function (supportingUnit) {
+									rl.question(`to where? `, function (dest) {
+										ordersObj.origin = territory;
+										ordersObj.destination = dest;
+										ordersObj.supportingUnit = supportingUnit;
+										ordersObj.actionType = dest === territory ? "H" : "M";
+										ordersObj.unitType = unitType;
+										ordersValidator(ordersObj);
+										rl.close();
+									});
 								});
-							});
-						}
-						if (action.toUpperCase() === "M") {
-							rl.question(
-								`Move where: ${availableMovements(territory, unitType)}? `,
-								function (dest) {
-									ordersObj.origin = territory;
-									ordersObj.destination = dest;
-									ordersObj.unitType = unitType;
-									ordersValidator(ordersObj);
-									rl.close();
-								}
-							);
+							}
+							if (action.toUpperCase() === "M") {
+								rl.question(
+									`Move the ${unitType} where: ${availableMovements(
+										territory,
+										unitType
+									)}? `,
+									function (dest) {
+										ordersObj.origin = territory;
+										ordersObj.destination = dest;
+										ordersObj.unitType = unitType;
+										ordersValidator(ordersObj);
+										rl.close();
+									}
+								);
+							}
 						}
 					}
-				}
-			);
-		}
-		if (
-			isTerritoryValid(territory) &&
-			requiresCoastInput(territory) &&
-			unitType === "N"
-		) {
-			rl.question("What coast? ", function (coast) {
-				console.log(
-					"available spaces ",
-					availableMovements(territory, unitType, coast)
 				);
-				rl.close();
-			});
+			}
+			if (
+				isTerritoryValid(territory) &&
+				requiresCoastInput(territory) &&
+				unitType === "N"
+			) {
+				rl.question("What coast? ", function (coast) {
+					console.log(
+						"available spaces ",
+						availableMovements(territory, unitType, coast)
+					);
+					rl.close();
+				});
+			}
 		}
-	});
+	);
 });
 
 rl.on("close", () => {
