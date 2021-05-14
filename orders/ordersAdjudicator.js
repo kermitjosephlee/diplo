7e510a806805d0302c3e4adad2109bdab6845c4f
Joe Lee <jlee@knitpeople.com>
2021-05-13 22:54:25 -0400

    updated adjudicator

--- orders/ordersAdjudicator.js
+++ orders/ordersAdjudicator.js
@@ -4,13 +4,12 @@ function ordersAdjudicator() {
 	// receive a list of orders
 	const gameId = 1;
 	const gameFile = `./orders/pending/game${gameId}.txt`;
-
 	const gameFileObj = JSON.parse(fs.readFileSync(gameFile, "utf-8"));
-
 	const orders = gameFileObj.gameId === gameId ? gameFileObj.orders : [];
 
 	console.log("orders", orders);
 	// validate move, support and convoy orders
+
 	// parse orders by type (move, support, hold, convoy)
 	// bundle move orders and hold orders
 	// validate support orders
