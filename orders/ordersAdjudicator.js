aadbbe894fddf9d60ba317d982f6d466f695c0d4
Joe Lee <jlee@knitpeople.com>
2021-05-21 11:22:57 -0400

    updated todo for orderAdjudicator

--- orders/ordersAdjudicator.js
+++ orders/ordersAdjudicator.js
@@ -43,12 +43,13 @@ function ordersAdjudicator() {
 		convoyValidatedHolds,
 	});
 
-	// validate move, support and convoy orders
+	//// validate move, support and convoy orders
 
-	// parse orders by type (move, support, hold, convoy)
-	// bundle move orders and hold orders
-	// validate support orders
-	// append support orders to move/hold orders
+	//// parse orders by type (move, support, hold, convoy)
+	//// bundle move orders and hold orders
+	//// validate support orders
+	//// append support orders to move/hold orders
+
 	// evaluate which support orders are cancelled by move orders
 	// score move orders
 	// evaluate successful move results
