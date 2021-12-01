7595d0770bc4efea8081eda707c4b433ba0e1d21
Joe Lee <kjlee@flipgive.com>
2021-12-01 07:25:59 -0500

    updated port console

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+const dotenv = require("dotenv").config()
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3001
+const { Client } = require("pg")
+const client = new Client({
+	connectionString: process.env.DATABASE_CONNECTION_STRING,
+	ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 app.get("/", (req, res) => {
 	res.send("Hello Diplomacy!");
 });
 
+client.connect()
+client.query('SELECT * FROM users;', (err, res) => {
+	console.log("err", err, "\nres", res)
+	client.end()
+})
+
 if (!module.parent) {
-	app.listen(3000);
-	console.log("Express started on port 3000");
+	app.listen(PORT);
+	console.log(`Express started on port ${PORT}`);
 }
