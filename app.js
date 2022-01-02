69e8e3a0474c7c30f4c0729cf147974c3ac51b81
Joe Lee <96074801+gl-kermitjosephlee@users.noreply.github.com>
2022-01-02 13:43:11 -0500

    removed pg client code from server

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ const dotenv = require("dotenv").config()
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001
-const { Client } = require("pg")
-// const client = new Client({
-// 	connectionString: process.env.DATABASE_CONNECTION_STRING,
-// 	ssl: {
-//     rejectUnauthorized: false
-//   }
-// })
 
 app.get("/", (req, res) => {
 	res.send("Hello Diplomacy!");
@@ -16,11 +9,6 @@ app.get("/", (req, res) => {
 
 console.log("database uri:", process.env.DATABASE_URL)
 
-// client.connect()
-// client.query('SELECT * FROM users;', (err, res) => {
-// 	console.log("err", err, "\nres", res)
-// 	client.end()
-// })
 
 if (!module.parent) {
 	app.listen(PORT);
