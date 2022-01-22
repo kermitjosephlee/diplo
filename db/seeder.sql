3d8dab1415eaa750505b26945b949cfba3ab3696
Joe Lee <kermitjosephlee@gmail.com>
2022-01-22 00:32:52 -0500

    verified create and initial insert seed works

--- db/seeder.sql
+++ db/seeder.sql
@@ -1,76 +1,55 @@
 CREATE TABLE
-    unit_types (id SERIAL PRIMARY KEY, unit_type CHAR(4) NOT NULL);
+    IF NOT EXISTS unit_types (id SERIAL PRIMARY KEY, unit_type CHAR(4) NOT NULL);
 
 INSERT INTO
     unit_types (unit_type)
 VALUES ('ARMY'), ('NAVY');
 
 CREATE TABLE
-    actions (id SERIAL PRIMARY KEY, "action" VARCHAR(12) NOT NULL);
+    IF NOT EXISTS actions (id SERIAL PRIMARY KEY, "action" VARCHAR(12) NOT NULL);
 
 INSERT INTO
     actions ("action")
 VALUES ('HOLD'), ('MOVE'), ('SUPPORT'), ('CONVOY');
 
 CREATE TABLE
-    phases (id SERIAL PRIMARY KEY, phase VARCHAR(12) NOT NULL);
+    IF NOT EXISTS phases (id SERIAL PRIMARY KEY, phase VARCHAR(12) NOT NULL);
 
 INSERT INTO
     phases (phase)
 VALUES ('MOVEMENT'), ('RETREAT'), ('DISBAND'), ('BUILD');
 
 CREATE TABLE
-    unit_types (id SERIAL PRIMARY KEY, unit_type CHAR(4) NOT NULL);
-
-INSERT INTO
-    unit_types (unit_type)
-VALUES ('ARMY'), ('NAVY');
-
-CREATE TABLE
-    actions (id SERIAL PRIMARY KEY, "action" VARCHAR(12) NOT NULL);
-
-INSERT INTO
-    actions ("action")
-VALUES ('HOLD'), ('MOVE'), ('SUPPORT'), ('CONVOY');
-
-CREATE TABLE
-    phases (id SERIAL PRIMARY KEY, phase VARCHAR(12) NOT NULL);
-
-INSERT INTO
-    phases (phase)
-VALUES ('MOVEMENT'), ('RETREAT'), ('DISBAND'), ('BUILD');
-
-CREATE TABLE
-    seasons (id SERIAL PRIMARY KEY, season VARCHAR(12) NOT NULL);
+    IF NOT EXISTS seasons (id SERIAL PRIMARY KEY, season VARCHAR(12) NOT NULL);
 
 INSERT INTO
     seasons (season)
 VALUES ('SPRING'), ('SUMMER'), ('FALL'), ('WINTER');
 
 CREATE TABLE
-    users (
+    IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         first_name TEXT,
         last_name TEXT,
         "user_name" TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL,
         email TEXT NOT NULL UNIQUE,
-        created_on TIMESTAMPTZ DEFAULT CURRENT_TIME,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
         is_active BOOLEAN DEFAULT TRUE
     );
 
 CREATE TABLE
-    games (
+    IF NOT EXISTS games (
         id SERIAL PRIMARY KEY,
         "name" TEXT NOT NULL,
-        created_on TIMESTAMPTZ DEFAULT CURRENT_TIME,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
         started_on TIMESTAMPTZ,
         next_turn_due_on TIMESTAMPTZ,
         ended_on TIMESTAMPTZ
     );
 
 CREATE TABLE
-    countries (
+    IF NOT EXISTS countries (
         id SERIAL PRIMARY KEY,
         "name" TEXT NOT NULL,
         abbreviation CHAR(3) NOT NULL,
@@ -82,10 +61,10 @@ INSERT INTO
 VALUES ('Austria', 'AUS', 'Austrian'), ('England', 'ENG', 'English'), ('France', 'FRA', 'French'), ('Germany', 'GER', 'German'), ('Italy', 'ITA', 'Italian'), ('Russia', 'RUS', 'Russian'), ('Turkey', 'TUR', 'Turkish');
 
 CREATE TABLE
-    maps (id SERIAL PRIMARY KEY, svg TEXT NOT NULL);
+    IF NOT EXISTS maps (id SERIAL PRIMARY KEY, svg TEXT NOT NULL);
 
 CREATE TABLE
-    locations (
+    IF NOT EXISTS locations (
         id SERIAL PRIMARY KEY,
         "name" TEXT NOT NULL,
         short_name TEXT NOT NULL,
@@ -99,7 +78,7 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    borders (
+    IF NOT EXISTS borders (
         ID SERIAL PRIMARY KEY,
         location_a INTEGER NOT NULL,
         location_b INTEGER NOT NULL,
@@ -111,7 +90,7 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    coasts (
+    IF NOT EXISTS coasts (
         id SERIAL PRIMARY KEY,
         location_id INTEGER NOT NULL,
         "position" VARCHAR(6) NOT NULL,
@@ -120,7 +99,7 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    coast_borders (
+    IF NOT EXISTS coast_borders (
         id SERIAL PRIMARY KEY,
         coast_id INTEGER NOT NULL,
         border_id INTEGER NOT NULL,
@@ -131,19 +110,19 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    service_providers(
+    IF NOT EXISTS service_providers(
         id SERIAL PRIMARY KEY,
         name TEXT NOT NULL,
         url TEXT NOT NULL
     );
 
 CREATE TABLE
-    user_chat_services (
+    IF NOT EXISTS user_chat_services (
         id SERIAL PRIMARY KEY,
         "user_id" INTEGER NOT NULL,
         service_provider_id INTEGER NOT NULL,
         service_provider_user_id INTEGER NOT NULL,
-        created_at TIMESTAMPTZ DEFAULT CURRENT_TIME,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
         last_used_at TIMESTAMPTZ,
         CONSTRAINT fk_user_id FOREIGN KEY ("user_id") REFERENCES users (id)
         ON DELETE NO ACTION,
@@ -152,12 +131,12 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    players (
+    IF NOT EXISTS players (
         id SERIAL PRIMARY KEY,
-        'user_id' INTEGER NOT NULL,
+        "user_id" INTEGER NOT NULL,
         country_id INTEGER NOT NULL,
         game_id INTEGER NOT NULL,
-        created_at TIMESTAMPTZ DEFAULT CURRENT_TIME,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
         ended_at TIMESTAMPTZ,
         is_winner BOOLEAN,
         CONSTRAINT fk_user_id FOREIGN KEY ("user_id") REFERENCES users (id)
@@ -175,9 +154,9 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    turns (
+    IF NOT EXISTS turns (
         id SERIAL PRIMARY KEY,
-        created_at TIMESTAMPTZ DEFAULT CURRENT_TIME NOT NULL,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
         finalized_at TIMESTAMPTZ,
         game_id INTEGER NOT NULL,
         "year" INTEGER NOT NULL,
@@ -197,7 +176,7 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    map_locations (
+    IF NOT EXISTS map_locations (
         id SERIAL PRIMARY KEY,
         map_id INTEGER NOT NULL,
         locations_id INTEGER NOT NULL,
@@ -211,7 +190,7 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    units (
+    IF NOT EXISTS units (
         id SERIAL PRIMARY KEY,
         unit_type_id INTEGER NOT NULL,
         location_id INTEGER NOT NULL,
@@ -219,7 +198,7 @@ CREATE TABLE
         coast_id INTEGER,
         CONSTRAINT fk_unit_type_id FOREIGN KEY (unit_type_id) REFERENCES unit_types (id)
         ON DELETE NO ACTION,
-        CONSTRAINT fk_location_id FOREIGN KEY (location_id) REFERENCES location (id)
+        CONSTRAINT fk_location_id FOREIGN KEY (location_id) REFERENCES locations (id)
         ON DELETE NO ACTION,
         CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES players (id)
         ON DELETE NO ACTION,
@@ -228,9 +207,9 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    orders (
+    IF NOT EXISTS orders (
         id SERIAL PRIMARY KEY,
-        created_at TIMESTAMPTZ DEFAULT CURRENT_TIME,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
         player_id INTEGER NOT NULL,
         turn_id INTEGER NOT NULL,
         unit_id INTEGER NOT NULL,
@@ -245,7 +224,7 @@ CREATE TABLE
         ON DELETE NO ACTION,
         CONSTRAINT fk_destination_id FOREIGN KEY (destination_id) REFERENCES locations (id)
         ON DELETE NO ACTION,
-        CONSTRAINT fk_supporting_unit_id FOREIGN KEY (supporting_unit_id) REFERENCES units (id)
+        CONSTRAINT fk_supporting_unit_id FOREIGN KEY (giving_support_to_unit_id) REFERENCES units (id)
         ON DELETE NO ACTION,
         CONSTRAINT fk_action_id FOREIGN KEY (action_id) REFERENCES actions (id)
         ON DELETE NO ACTION
