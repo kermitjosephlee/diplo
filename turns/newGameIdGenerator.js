59cc8264a40d177af2db5bdff287fe6d667ee209
Joe Lee <kermitjosephlee@gmail.com>
2022-01-23 09:54:12 -0500

    db query helper file

--- turns/newGameIdGenerator.js
+++ turns/newGameIdGenerator.js
@@ -1,8 +1,17 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-function newGameIdGenerator() {
-	const turns = fs.readdirSync(`./turns/currentGames`);
-	return turns.length + 1;
-}
+// function newGameIdGenerator() {
+// 	const turns = fs.readdirSync(`./turns/currentGames`);
+// 	return turns.length + 1;
+// }
 
-exports.newGameIdGenerator = newGameIdGenerator;
+// exports.newGameIdGenerator = newGameIdGenerator;
+
+const db = require('../db/index')
+
+db.query(`SELECT COUNT (*) FROM games;` , null, (err, res) => {
+	if (err) console.log (err)
+
+	console.log(res.rows)
+
+})
