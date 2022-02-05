039b3572c1221e7abddde399eb3f89c1dd7edcee
Joe Lee <kermitjosephlee@gmail.com>
2022-02-05 09:57:37 -0500

    playing around with readline and db query

--- orders/helpers/isTerritoryValid.js
+++ orders/helpers/isTerritoryValid.js
@@ -1,10 +1,26 @@
-const { gameMap } = require("../../constants/gameMap");
+// const { gameMap } = require("../../constants/gameMap");
+const { reject } = require('lodash')
+const db = require('../../db')
 
-const names = gameMap.map(({ name }) => name);
-const shortNames = gameMap.map(({ shortName }) => shortName);
+// const names = gameMap.map(({ name }) => name);
+// const shortNames = gameMap.map(({ shortName }) => shortName);
+
+
 
 function isTerritoryValid(territory) {
-	return names.includes(territory) || shortNames.includes(territory);
+	const selectQuery = `SELECT * FROM locations WHERE name = $1 OR short_name = $1;`
+	const variables = [territory]
+
+	db.query(selectQuery, variables)
+	.then((data) => {
+		// console.log(data)
+		return data.rowCount === 1 && data.rows.length === 1
+	})
+	.catch(e => {
+		console.log(e)
+		return false
+	})
+	.finally(() => db.end())
 }
 
 exports.isTerritoryValid = isTerritoryValid;
--- orders/inputPrompt.js
+++ orders/inputPrompt.js
@@ -1,69 +1,98 @@
+const db = require("../db")
 const readline = require("readline");
+const { isTerritoryValid } = require("./helpers/isTerritoryValid");
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 });
 
-const {
-	isTerritoryValid,
-	requiresCoastInput,
-	isActionValid,
-	unitFinder,
-	holdActionHandler,
-	convoyActionHandler,
-	moveActionHandler,
-	supportActionHandler,
-	coastalExceptionHandler,
-} = require("./helpers");
+// const {
+// 	isTerritoryValid,
+// 	requiresCoastInput,
+// 	isActionValid,
+// 	unitFinder,
+// 	holdActionHandler,
+// 	convoyActionHandler,
+// 	moveActionHandler,
+// 	supportActionHandler,
+// 	coastalExceptionHandler,
+// } = require("./helpers");
 
-const { nationalAdjectives } = require("../constants/nationalAdjectives");
+// const { nationalAdjectives } = require("../constants/nationalAdjectives");
 
-function inputPrompt() {
-	// prompt for unit in question by territory
+function inputPrompt(){
+	rl.question("What is the location of your unit? ", 
+		(territory) => {
+			const selectQuery = `SELECT * FROM locations WHERE name = $1 OR short_name = $1;`
+			const variables = [territory]
+		
+			db.query(selectQuery, variables)
+			.then((data) => {
+				
+				if (data.rowCount === 1 && data.rows.length === 1){
+					rl.question("Where are we going? ", (destination) => {
 
-	rl.question("What is the location of the unit? ", (territory) => {
-		if (!isTerritoryValid(territory)) {
-			console.log(`There is no unit available in ${territory} - try again`);
-			rl.close();
-		}
+						const destinationQuery = `SELECT * FROM locations WHERE name = $1 or short_name = $1;`
+						const destinationVariables = [destination]
 
-		const currentUnit = unitFinder(territory);
-		const { location, unitType, nation } = currentUnit;
-		const nationalAdjective = nationalAdjectives[nation];
 
-		// check if territory entered does not need coast input
-		if (!requiresCoastInput(territory)) {
-			// prompt for action type
-			rl.question(
-				`What would you like the ${nationalAdjective} ${unitType} in ${location} to do?\n(M)ove, (H)old, (C)onvoy, (S)upport? `,
-				(action) => {
-					if (isActionValid(action.toUpperCase())) {
-						switch (action.toUpperCase()) {
-							case "H":
-								holdActionHandler(location, unitType, nation, rl);
-							case "C":
-								convoyActionHandler(location, unitType, nation, rl);
-							case "S":
-								supportActionHandler(location, unitType, nation, rl);
-							case "M":
-								moveActionHandler(location, unitType, nation, rl);
-						}
-					} else {
-						console.log("no orders issued");
-						rl.close();
-					}
+						db.query(destinationQuery, destinationVariables)
+						.then(data => {
+							console.log("data...", data)
+						})
+					})
 				}
-			);
+			})
 		}
-
-		if (requiresCoastInput(location) && unitType === "Navy") {
-			coastalExceptionHandler(location, unitType, nation, rl);
-		}
-	});
-
-	rl.on("close", () => {
-		process.exit(0);
-	});
+	)
 }
+
+// function inputPrompt() {
+// 	// prompt for unit in question by territory
+
+// 	rl.question("What is the location of the unit? ", (territory) => {
+// 		if (!isTerritoryValid(territory)) {
+// 			console.log(`There is no unit available in ${territory} - try again`);
+// 			rl.close();
+// 		}
+
+// 		const currentUnit = unitFinder(territory);
+// 		const { location, unitType, nation } = currentUnit;
+// 		const nationalAdjective = nationalAdjectives[nation];
+
+// 		// check if territory entered does not need coast input
+// 		if (!requiresCoastInput(territory)) {
+// 			// prompt for action type
+// 			rl.question(
+// 				`What would you like the ${nationalAdjective} ${unitType} in ${location} to do?\n(M)ove, (H)old, (C)onvoy, (S)upport? `,
+// 				(action) => {
+// 					if (isActionValid(action.toUpperCase())) {
+// 						switch (action.toUpperCase()) {
+// 							case "H":
+// 								holdActionHandler(location, unitType, nation, rl);
+// 							case "C":
+// 								convoyActionHandler(location, unitType, nation, rl);
+// 							case "S":
+// 								supportActionHandler(location, unitType, nation, rl);
+// 							case "M":
+// 								moveActionHandler(location, unitType, nation, rl);
+// 						}
+// 					} else {
+// 						console.log("no orders issued");
+// 						rl.close();
+// 					}
+// 				}
+// 			);
+// 		}
+
+// 		if (requiresCoastInput(location) && unitType === "Navy") {
+// 			coastalExceptionHandler(location, unitType, nation, rl);
+// 		}
+// 	});
+
+// 	rl.on("close", () => {
+// 		process.exit(0);
+// 	});
+// }
 
 exports.inputPrompt = inputPrompt;
